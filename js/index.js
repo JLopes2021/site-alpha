cbf1eb48d58ad6d184c347b12ddb101d5ba56bea
Jefferson Lopes <81269229+JLopes2021@users.noreply.github.com>
2022-07-23 02:49:56 -0300

    projeto-finalizado

--- js/index.js
+++ js/index.js
@@ -1,12 +1,6 @@
 function abrirMensagens() {
     let email_pessoa = ""
     let senha_pessoa = ""
-
-    var confirmacao = prompt(" Seu nome é " + nome + " e seu email é " + email + " ?" + " Digite 1 para SIM ou DIGITE 2 para NÃO")
-    if (confirmacao === 2){
-        alert("Refaça os passos anteriores")
-        return
-    }
     
     if (email === email_pessoa ){
         alert("Ok, você está cadastrado(a) no sistema!") 
